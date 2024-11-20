dc0c6ea30fc81bd8053186d81d1acca4f4ee1f79
elcosas <121999809+elcosas@users.noreply.github.com>
2024-11-19 18:43:56 -0800

    added stuff

--- app/app.py
+++ app/app.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template, request, redirect
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+
+@app.route('/', methods=['GET', 'POST'])
+def index() -> str:
+    if request.method == 'POST':
+        pass
     return render_template('index.html')
 
+
 @app.route('/about')
-def about():
+def about() -> str:
     return render_template('about.html')
+
+
+# Helper functions
+def get_cached_data(location: str, meal) -> list[str]:
+    #TODO: query data from zotmeal-backend
+    return menu_list
