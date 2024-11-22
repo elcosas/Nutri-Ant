b155e19e2ff637b18753296c02460d846ea7c262
Sean Hoang <sbhoang@uci.edu>
2024-11-21 19:03:31 -0800

    Done with basic front end and interaction with backend. Waiting for implementatino of object which connects to zot meal api

--- app/app.py
+++ app/app.py
@@ -31,14 +31,27 @@ def main_page():
 
 @app.route('/submit-health-data', methods=['POST'])
 def submit_health_data():
-    # Parse the JSON data sent by the client
+    # TODO: Parse the JSON data sent by the client
 
     data = request.get_json()
-    print(json.dumps(data, indent = 4))
+    print(json.dumps(data, indent = 4)) # just printing out to terminal
     # Perform backend logic (e.g., process form data, save to DB)
     if not data:
         return jsonify({'error': 'Invalid data'}), 400
 
-    # Example: Return an HTML snippet based on data
-    # processed_data = "Processed: " + str(data.get('health_metric', 'unknown'))
-    return render_template('about.html')
+    # TODO: Item_list = some_function_here
+    # TODO: return render_template('about.html', items = item_list)
+    
+    return render_template('about.html', items = [item() for _ in range(10)])
+
+# currently just for testing 
+class item():
+    def __init__(self):
+        self.f1 = 'hii'
+        self.f2 = 'hii'
+        self.f3 = 'hii'
+        self.f4 = 'hii'
+        self.f5 = 'hii'
+    
+    def __repr__(self):
+        return f'Hi I am an item at {id(self)}'
